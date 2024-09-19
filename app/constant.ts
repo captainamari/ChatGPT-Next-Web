77ec21b61085c7cb3892600c73eb1427de675ffb
captainamari <109799765+captainamari@users.noreply.github.com>
2024-09-19 17:10:58 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -235,7 +235,7 @@ Latex block: $$e=mc^2$$
 `;
 
 // export const SUMMARIZE_MODEL = "gpt-4o-mini";
-export const SUMMARIZE_MODEL = "gpt-4o";
+export const SUMMARIZE_MODEL = "gpt-4o@azure=4otrial";
 export const GEMINI_SUMMARIZE_MODEL = "gemini-pro";
 
 export const KnowledgeCutOffDate: Record<string, string> = {
